a2f1a6bc6dc66e12f208d70c21f733fd83924c34
Shahanawaz <shahanawazkhan579@gmail.com>
2023-09-08 18:18:39 +0530

    created data folder for all data

---
+++ src/data/Data.js
@@ -0,0 +1,107 @@
+import iNotebook from "../assets/img/iNotebook.png";
+import NewsTime from "../assets/img/NewsTime.png";
+import textUtils from "../assets/img/textUtils.png";
+import Portfolio from "../assets/img/Portfolio.png";
+import DigitalClock from "../assets/img/DigitalClock.png";
+import DigitalMedia from "../assets/img/DigitalMedia.png";
+import eCommerceWebApp from "../assets/img/eCommerceWebApp.png";
+import passwordGenerator from "../assets/img/passwordGenerator.png";
+import colorPicker from "../assets/img/colorPicker.png";
+import psdToHtml from "../assets/img/psdToHtml.png";
+import fitbit from "../assets/img/fitbit laptop.png";
+
+export const projectsReactBootstrap = [
+  {
+    title: "FitBit",
+    description:
+      "FitBit is a state-of-the-art gym website, meticulously crafted with React.js and Bootstrap, reflects our commitment to providing you with the best fitness experience possible. Completely responsive for different screen sizes.",
+    imgUrl: fitbit,
+    gitHubRepo: "https://github.com/Shahanawazgit/fitbit",
+    liveLink: "https://fitbit-nine.vercel.app/",
+  },
+  {
+    title: "Shahanawaz's Portfolio",
+    description:
+      "Welcome to my portfolio website, a digital playground where creativity meets code! I am a passionate frontend developer, and this is where I showcase my skills, projects, and journey through the ever-evolving world of web development. Join me on this visual and interactive journey through the websites and applications I've crafted with care.",
+    imgUrl: Portfolio,
+    gitHubRepo: "https://github.com/Shahanawazgit/ShahanawazPortfolio",
+    liveLink: "https://shahanawaz-portfolio.vercel.app/",
+  },
+  {
+    title: "iNotebook",
+    description:
+      "iNotebook is a sleek and user-friendly MongoDB, Express.js, React.js & Node.js based web application designed to simplify and enhance your note-taking experience. Whether you're a student, professional, or just someone who loves to stay organized, iNotebook offers a versatile platform for capturing, managing, and organizing your thoughts, ideas, and important information.",
+    imgUrl: iNotebook,
+    gitHubRepo: "https://github.com/Shahanawazgit/iNotebook",
+    liveLink: "https://i-notebook-topaz.vercel.app/",
+  },
+  {
+    title: "NewsTime",
+    description:
+      "Welcome to NewsTime, your one-stop destination for staying informed about the latest news and current events. Powered by the cutting-edge technology of React.js and the expansive capabilities of the NewsAPI, I have created a dynamic and user-friendly news web application that puts the world's headlines at your fingertips.",
+    imgUrl: NewsTime,
+    gitHubRepo: "https://github.com/Shahanawazgit/NewsTime",
+    liveLink: "https://github.com/Shahanawazgit/NewsTime",
+  },
+  {
+    title: "TextUtils",
+    description:
+      "TextUtils is the go-to web application for all your text manipulation needs, meticulously crafted with React.js to provide you with a seamless and powerful experience. Whether you're a writer, coder, marketer, or simply someone who wants to manipulate a text, TextUtils has got you covered.",
+    imgUrl: textUtils,
+    gitHubRepo: "https://github.com/Shahanawazgit/TextUtils",
+    liveLink: "https://text-utils-blue.vercel.app/",
+  },
+];
+
+export const projectsHtmlCssJs = [
+  {
+    title: "Digital Media",
+    description:
+      "Digital Media - our Digital Marketing Agency, we understand the importance of staying at the forefront of the digital landscape. With our website as a prime example, we combine HTML's robust structure, CSS's elegant styling, and JavaScript's interactivity to deliver a digital experience that mirrors the innovative solutions we provide to our clients.",
+    imgUrl: DigitalMedia,
+    gitHubRepo: "https://github.com/Shahanawazgit/DigitalMedia",
+    liveLink: "https://shahanawazgit.github.io/DigitalMedia/",
+  },
+  {
+    title: "E-Commerce WebApp",
+    description:
+      "Welcome to our Ecommerce Web App Frontend, your gateway to a world of online shopping convenience and style. Our frontend has been meticulously crafted using a powerful combination of HTML, CSS, and JavaScript, ensuring a seamless and engaging shopping experience for our valued customers.",
+    imgUrl: eCommerceWebApp,
+    gitHubRepo: "https://github.com/Shahanawazgit/E-commerce-WebApp",
+    liveLink: "https://shahanawazgit.github.io/E-commerce-WebApp/",
+  },
+  {
+    title: "Password Generator",
+    description:
+      "Password Generator - the ultimate solution to ensure your online security. In today's digital age, safeguarding your personal information has never been more critical. My Password Generator Web App empowers you to create robust, complex, and virtually unhackable passwords effortlessly.",
+    imgUrl: passwordGenerator,
+    gitHubRepo: "https://github.com/Shahanawazgit/",
+    liveLink:
+      "https://69exercise-7-password-generator--shahanawaz-path.repl.co/",
+  },
+  {
+    title: "colorPicker",
+    description: "colorPicker made by using HTML CSS and JS",
+    imgUrl: colorPicker,
+    gitHubRepo: "https://github.com/Shahanawazgit/Color-Picker",
+    liveLink: "https://shahanawazgit.github.io/Color-Picker/",
+  },
+  {
+    title: "DigitalClock",
+    description: "Digital clock made by using HTML CSS and JS",
+    imgUrl: DigitalClock,
+    gitHubRepo: "https://github.com/Shahanawazgit/",
+    liveLink: "https://33exersice04-digital-clock--shahanawaz-path.repl.co/",
+  },
+];
+
+export const projectsHtmlCssBootstrapJs = [
+  {
+    title: "psdToHtml",
+    description:
+      "Transformed the static Photoshop design into a captivating online portfolio website with the power of HTML, CSS, Bootstrap, and JavaScript",
+    imgUrl: psdToHtml,
+    gitHubRepo: "https://github.com/Shahanawazgit/PSD-to-HTML",
+    liveLink: "https://shahanawazgit.github.io/PSD-to-HTML/",
+  },
+];
